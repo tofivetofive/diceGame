9c54f2ae795f3d0e69260071a0efb15ac3d5b362
Joshua Howard <102174406+tofivetofive@users.noreply.github.com>
2022-07-14 17:44:07 -0400

    Add files via upload
    Added winning player name above dice.

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 
-
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber2);
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
@@ -27,13 +26,16 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = (player1) + " Wins ✨✨! ! ";
-    
+    document.querySelectorAll("p")[0].innerHTML = player1
 }
 
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = (player2) + " Wins ✨✨! ! "
+    document.querySelector("h1").innerHTML = (player2) + " Wins ✨✨! ! ";
+    document.querySelectorAll("p")[1].innerHTML = player2;
 }
 
 else {
-    document.querySelector("h1").innerHTML = "✏ Draw ! ! ! ✏"
+    document.querySelector("h1").innerHTML = "✏ Draw ! ! ! ✏";
+    document.querySelectorAll("p")[0].innerHTML = player1;
+    document.querySelectorAll("p")[1].innerHTML = player2;
 }
